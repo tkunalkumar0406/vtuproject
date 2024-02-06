d3d1894e086c7911aefc5aedd1e4441973cf620b
t kunalkumar <tkunalkumar43@gmail.com>
2024-02-06 15:56:42 +0530

    Created Footer Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 
+import Footer from './components/Footer';
 import Header from './components/Header';
+import { Container } from "react-bootstrap";
 
 function App() {
   return (
+    <>
     <Header/>
+        <Container>
+             <main>
+
+             </main>
+        </Container>
+    <Footer/>
+    </>
   );
 }
 
